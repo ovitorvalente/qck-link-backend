8947579c0b06de53eb9934dc37c42bb832b8f1a2
Vitor Valente <ovitorvalente@gmail.com>
2025-05-14 22:54:39 -0300

    feat: adiciona informacoes de schema para a documentacao com swagger

--- src/routes/shorten.ts
+++ src/routes/shorten.ts
@@ -1,11 +1,26 @@
-import { FastifyInstance } from "fastify";
+import z from "zod";
 import { createShortUrl } from "../services/url.service";
+import { FastifyTypeInstance } from "../types";
 
-export async function ShortenRoute(app: FastifyInstance) {
-  app.post("/shorten", async (request, reply) => {
-    const { originalUrl } = request.body as { originalUrl: string };
-    const link = await createShortUrl(originalUrl);
+export async function ShortenRoute(app: FastifyTypeInstance) {
+  app.post(
+    "/shorten",
+    {
+      schema: {
+        description: "Criar um novo link encurtado",
+        tags: ["Link"],
+        body: z.object({
+          originalUrl: z.string().url(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { originalUrl } = request.body;
+      const link = await createShortUrl(originalUrl);
 
-    reply.send({ shortUrl: `${process.env.SERVER_URL}/${link.code}` });
-  });
+      reply
+        .status(201)
+        .send({ shortUrl: `${process.env.SERVER_URL}/${link.code}` });
+    }
+  );
 }
