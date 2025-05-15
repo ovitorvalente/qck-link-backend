d383570b26ef3fcb2d64fa6e4cc79d437bde39ae
Vitor Valente <ovitorvalente@gmail.com>
2025-05-14 22:55:14 -0300

    feat: adiciona informacoes de schema para a documentacao com swagger

--- src/routes/redirect.ts
+++ src/routes/redirect.ts
@@ -2,14 +2,23 @@ import { FastifyInstance } from "fastify";
 import { findOriginalUrl } from "../services/url.service";
 
 export async function RedirectRoute(app: FastifyInstance) {
-  app.get("/:code", async (request, reply) => {
-    const { code } = request.params as { code: string };
-    const link = await findOriginalUrl(code);
+  app.get(
+    "/:code",
+    {
+      schema: {
+        description: "Redireciona o código encurtado para a URL original",
+        tags: ["Link"],
+      },
+    },
+    async (request, reply) => {
+      const { code } = request.params as { code: string };
+      const link = await findOriginalUrl(code);
 
-    if (!link) {
-      return reply.status(404).send({ error: `Link not found` });
+      if (!link) {
+        return reply.status(404).send({ error: `Link not found` });
+      }
+
+      reply.redirect(link.originalUrl);
     }
-
-    reply.redirect(link.originalUrl);
-  });
+  );
 }
