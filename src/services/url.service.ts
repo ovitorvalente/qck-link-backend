326f43403f6482024559febc3bb73a5acee3b18c
Vitor Valente <ovitorvalente@gmail.com>
2025-05-17 16:57:49 -0300

    feat: adiciona parâmetros de iv e isEncrypted à função createShortUrl

--- src/services/url.service.ts
+++ src/services/url.service.ts
@@ -3,13 +3,21 @@ import { GenerateCode } from "../utils/generateCode";
 
 const prisma = new PrismaClient();
 
-export async function createShortUrl(originalUrl: string, userId?: string) {
+export async function createShortUrl(
+  originalUrl: string,
+  iv?: string,
+  isEncrypted?: boolean,
+  userId?: string
+) {
   const code = GenerateCode(6);
-  const data = userId ? { code, originalUrl, userId } : { code, originalUrl };
+  const data = userId
+    ? { code, originalUrl, iv, userId, isEncrypted }
+    : { code, originalUrl, iv, isEncrypted };
+
   const link = await prisma.link.create({ data });
   return link;
 }
 
-export async function findOriginalUrl(code: string) {
+export async function findUrl(code: string) {
   return prisma.link.findUnique({ where: { code } });
 }
