5a007757894228ab51cb6c9b5f9db7d8877dc700
Vitor Valente <ovitorvalente@gmail.com>
2025-05-19 18:34:32 -0300

    fix: corrige a importação do PrismaClient no serviço de URL

--- src/services/url.service.ts
+++ src/services/url.service.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { GenerateCode } from "../utils/generateCode";
 
 const prisma = new PrismaClient();
