be5031f08576a48c89c9a033b1730d73e8823417
Vitor Valente <ovitorvalente@gmail.com>
2025-05-19 18:08:29 -0300

    feat: atualiza a configuração do servidor para usar variáveis de ambiente e melhora a mensagem de log

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import { app } from "./app";
 
-app.listen({ port: 3333 }).then(() => {
-  console.log("Backend rodando");
-});
+app
+  .listen({
+    host: "0.0.0.0",
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server Running...");
+  });
